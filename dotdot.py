5a2b289454e0eab1786b7f2b3317b3a696805e88
Emil Kovacev <emilkova@buffalo.edu>
2021-02-28 01:16:02 -0500

    added texttofont ascii functionality

--- dotdot.py
+++ dotdot.py
@@ -3,36 +3,52 @@ import os
 import subprocess
 import discord
 from dotenv import load_dotenv
+from texttofont import word_to_ascii
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 """function chunks from ned batchelder on stackoverflow"""
+
+
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
-
     if message.author.bot:
         return
-    
-    if message.content[:6] == ":print":
+
+    if message.content[:7] == ":printf":
         tempy = message.content[6:]
         if tempy[0] == ' ':
             tempy = tempy[1:]
-        tempy = message.author.nick + ' said: ' + tempy 
+        tempy = word_to_ascii(tempy)
+        tempy = message.author.nick + ':\n' + tempy
         chunked = chunks(tempy, 85)
-        with open("/dev/usb/lp0","w") as outFile:
+        with open("/dev/usb/lp0", "w") as outFile:
             for line in chunked:
-                subprocess.run(["echo", line],stdout=outFile)
+                subprocess.run(["echo", line], stdout=outFile)
+        await message.channel.send("printing atof...")
+
+    elif message.content[:6] == ":print":
+        tempy = message.content[6:]
+        if tempy[0] == ' ':
+            tempy = tempy[1:]
+        tempy = message.author.nick + ' said: ' + tempy
+        chunked = chunks(tempy, 85)
+        with open("/dev/usb/lp0", "w") as outFile:
+            for line in chunked:
+                subprocess.run(["echo", line], stdout=outFile)
         await message.channel.send("printing...")
 
 client.run(TOKEN)
